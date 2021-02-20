a45072b0ad04f9a4cf9ab82549805df8f8b72168
BuildTools <unconfigured@null.spigotmc.org>
2021-02-20 12:31:43 +0100

    +File AsyncChatListener

---
+++ src/de/lama/banmanager/listeners/AsyncChatListener.java
@@ -0,0 +1,14 @@
+package de.lama.banmanager.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class AsyncChatListener implements Listener {
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+
+    }
+
+}
